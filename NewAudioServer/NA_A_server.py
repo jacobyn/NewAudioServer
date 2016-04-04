8c0a48fcfac2a17cfcb576a5152adde5f651ab48
jacobyn <nori.viola@gmail.com>
2016-04-03 20:56:26 -0700

    fix anohte bug3

--- NewAudioServer/NA_A_server.py
+++ NewAudioServer/NA_A_server.py
@@ -38,6 +38,7 @@ def analyze():
         #print request.method
         file = request.files['file']
         temp_fname=os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        temp_fname='stam.wav'
         file.save(temp_fname)
 
         return Response(dumps({'filename': filename}), status=200, mimetype='application/json')
